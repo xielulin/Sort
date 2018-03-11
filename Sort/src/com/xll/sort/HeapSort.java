ad2cc6149ce343578072db4288ed15a386c7a805
youname <fengshiyeyileng@163.com>
2018-03-11 11:34:46 +0800

    堆排序的实现

--- Sort/src/com/xll/sort/HeapSort.java
+++ Sort/src/com/xll/sort/HeapSort.java
@@ -25,6 +25,56 @@ public class HeapSort {
 	* @throws
 	 */
 	private static void sort(int[] arr) {
+		for(int i=arr.length/2-1;i>=0;i--){//构建大顶堆,从第一个非叶子节点，从下至上调整
+			adjustHeap(arr, i, arr.length);
+		}
 		
+		for(int j=arr.length-1;j>0;j--){
+			swap(arr, 0, j);
+			adjustHeap(arr, 0, j);
+		}
+	}
+	
+	
+	
+	
+	/**  
+	* @Title: adjustHeap  
+	* @Description: 将数组调整为堆
+	* @param @param arr 待调整的数组
+	* @param @param i 当前待调整的元素
+	* @param @param length  待调整数组的待调整长度     
+	* @return void     
+	* @throws  
+	*/ 
+	private static void adjustHeap(int arr[],int i,int length){
+		int temp = arr[i];
+		for(int k=2*i+1;k<length;k=2*k+1){
+			if(k+1<length&&arr[k]<arr[k+1]){//如果左子节点小于右子节点，k指向右子节点
+				k++;
+			}
+			if(arr[k]>temp){
+				arr[i]=arr[k];
+				i=k;
+			}else{
+				break;
+			}
+		}
+		arr[i]=temp;
+	}
+	
+	/**  
+	* @Title: swap  
+	* @Description: 交换  
+	* @param @param arr 
+	* @param @param a
+	* @param @param b       
+	* @return void     
+	* @throws  
+	*/ 
+	private static void swap(int []arr,int a,int b){
+		int temp=arr[a];
+		arr[a]=arr[b];
+		arr[b]=temp;
 	}
 }
