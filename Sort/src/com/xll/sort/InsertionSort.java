0070d9024362f99d8320a0dad788ff2ed8e376a6
xielulin <656600528@qq.com>
2019-04-19 16:31:40 +0800

    format

--- Sort/src/com/xll/sort/InsertionSort.java
+++ Sort/src/com/xll/sort/InsertionSort.java
@@ -2,8 +2,10 @@ package com.xll.sort;
 
 import java.util.Arrays;
 
- /** 插入排序
- * @ClassName: InsertionSort 
+/**
+ * 插入排序
+ *
+ * @ClassName: InsertionSort
  * @Description: 从第一个元素开始，该元素可以认为已经被排序；
  * 取出下一个元素，在已经排序的元素序列中从后向前扫描；
  * 如果该元素（已排序）大于新元素，将该元素移到下一位置；
@@ -11,35 +13,34 @@ import java.util.Arrays;
  * 将新元素插入到该位置后；
  * 重复步骤2~5。
  * @author: XieLulin
- * @date: 2018年3月11日 下午3:46:38  
+ * @date: 2018年3月11日 下午3:46:38
  */
 public class InsertionSort {
-	public static void main(String[] args) {
-		int [] arr ={1,3,2,6,5,4,7,9,8,15,76,45,23};
-		sort(arr);
-		System.out.println(Arrays.toString(arr));
-	}
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 2, 6, 5, 4, 7, 9, 8, 15, 76, 45, 23};
+        sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
 
-	/**
-	 * 
-	* @Title: sort  
-	* @Description: 插入排序算法
-	* @param @param arr       
-	* @return void     
-	* @throws
-	 */
-	private static void sort(int[] arr) {
-		int length=arr.length;
-		for(int i=0;i<length-1;i++){
-			for(int j=i+1;j>0;j--){
-				if(j-1>=0&&arr[j]>arr[j-1]){
-					int temp=arr[j];
-					arr[j]=arr[j-1];
-					arr[j-1]=temp;
-				}else{
-					break;
-				}
-			}
-		}
-	}
+    /**
+     * @param @param arr
+     * @return void
+     * @throws
+     * @Title: sort
+     * @Description: 插入排序算法
+     */
+    private static void sort(int[] arr) {
+        int length = arr.length;
+        for (int i = 0; i < length - 1; i++) {
+            for (int j = i + 1; j > 0; j--) {
+                if (j - 1 >= 0 && arr[j] > arr[j - 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
+                } else {
+                    break;
+                }
+            }
+        }
+    }
 }
--- Sort/src/com/xll/sort/SelectionSort.java
+++ Sort/src/com/xll/sort/SelectionSort.java
@@ -4,7 +4,8 @@ import java.util.Arrays;
 
 /**
  * 选择排序
- * @ClassName: SelectionSort 
+ *
+ * @ClassName: SelectionSort
  * @Description: 将无序中最大或最小的插入到有序队列的末尾
  * 时间复杂度：最佳情况：T(n) = O(n2)  最差情况：T(n) = O(n2)  平均情况：T(n) = O(n2)
  * 最稳定的排序算法
@@ -12,31 +13,31 @@ import java.util.Arrays;
  * @date: 2018年3月11日 下午2:57:40
  */
 public class SelectionSort {
-	public static void main(String[] args) {
-		int [] arr ={1,3,2,6,5,4,7,9,8};
-		sort(arr);
-		System.out.println(Arrays.toString(arr));
-	}
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 2, 6, 5, 4, 7, 9, 8};
+        sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
 
-	/**  
-	* @Title: sort  
-	* @Description: 选择排序                                                                                                                                                                                                 																																																																																																																																																																																																																																																																																																																																																																																																																																																																								                                                     排序
-	* @param @param arr       
-	* @return void     
-	* @throws  
-	*/ 
-	private static void sort(int[] arr) {
-		int length=arr.length;
-		for(int i=0;i<length-1;i++){
-			int index=i;
-			for(int j=i+1;j<length;j++){
-				 if(arr[index]>arr[j]){//选出最小值的位置
-					 index=j;
-				 }
-			}
-			int temp=arr[i];
-			arr[i]=arr[index];
-			arr[index]=temp;
-		}
-	}
+    /**
+     * @param @param arr
+     * @return void
+     * @throws
+     * @Title: sort
+     * @Description: 选择排序                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     排序
+     */
+    private static void sort(int[] arr) {
+        int length = arr.length;
+        for (int i = 0; i < length - 1; i++) {
+            int index = i;
+            for (int j = i + 1; j < length; j++) {
+                if (arr[index] > arr[j]) {//选出最小值的位置
+                    index = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[index];
+            arr[index] = temp;
+        }
+    }
 }
